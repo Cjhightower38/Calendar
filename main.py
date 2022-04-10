754b2c2ac89833bff3638911434480181277e19f
Cornelius Jermaine Hightower <75323116+Cjhightower38@users.noreply.github.com>
2022-04-10 22:48:48 +0000

    add proper docstrings

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-# This function determines if a year is leap or not and returns a boolean.
 def is_leap(year):
+  '''
+  This function determines if a year is leap or not and returns a boolean.
+  '''
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
@@ -10,11 +12,12 @@ def is_leap(year):
       return True
   else:
     return False
-'''
+
+def days_in_month(year,month):
+  '''
 This function takes the results from the is_leap function and return 29 days if it is a leap year. Becareful with the
 months off by 1. A user will not enter off by one so month -1 corrects the problem. Lists start at 0
 '''
-def days_in_month(year,month):
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
   if is_leap(year) and month == 2:
     return 29
